1460d9fca69396c35e9a45a539b5847465a36d90
Leo <biminglei@gmail.com>
2015-06-09 13:35:50 +0800

    update

--- aquila/src/club/magicfun/aquila/DemoJsonCrawler.java
+++ aquila/src/club/magicfun/aquila/DemoJsonCrawler.java
@@ -1,5 +1,8 @@
 package club.magicfun.aquila;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.json.JSONObject;
 
 import cn.edu.hfut.dmic.webcollector.crawler.DeepCrawler;
@@ -22,27 +25,57 @@ public class DemoJsonCrawler extends DeepCrawler {
 	@Override
 	public Links visitAndGetNextLinks(Page page) {
 
-		String jsonStr = page.getHtml();
-		System.out.println(jsonStr);
+		String jsonpStr = page.getHtml();
+		//System.out.println(jsonpStr);
+		
+		String jsonStr = jsonpStr.substring(12, jsonpStr.length() - 1);
+		System.out.println((unicodeToString(jsonStr).replace("\t", "").replace(" ", "")));
+		//System.out.println(convert(jsonStr).replace("\t", ""));
+		
+		//JSONObject json = new JSONObject(jsonStr);
+		//System.out.println(json);
 		
 		return null; 
+	}
+	
+	private String convert(String utfString){
+		StringBuilder sb = new StringBuilder();
+		int i = -1;
+		int pos = 0;
 		
-		/*
-		JSONObject json = new JSONObject(jsonStr);
-		String ip = json.getString("ip");
-		int port = json.getInt("port");
-		System.out.println("JSON:" + jsonStr.trim() + "\n" + "JSON ip=" + ip + "  port=" + port);
-		return null;
-		*/
+		while((i=utfString.indexOf("\\u", pos)) != -1){
+			sb.append(utfString.substring(pos, i));
+			if(i+5 < utfString.length()){
+				pos = i+6;
+				sb.append((char)Integer.parseInt(utfString.substring(i+2, i+6), 16));
+			}
+		}
+		
+		return sb.toString();
+	}
+	
+
+	private static String unicodeToString(String str) {
+	    Pattern pattern = Pattern.compile("(\\\\u(\\p{XDigit}{4}))");    
+	    Matcher matcher = pattern.matcher(str);
+	    char ch;
+	    while (matcher.find()) {
+	        ch = (char) Integer.parseInt(matcher.group(2), 16);
+	        str = str.replace(matcher.group(1), ch + "");    
+	    }
+	    return str;
 	}
 
 	public static void main(String[] args) throws Exception {
 
-		DemoJsonCrawler crawler = new DemoJsonCrawler("/home/hu/data/wb");
-		crawler.addSeed("http://tui.taobao.com/recommend?sellerid=109725928&itemid=27563020249&buyerid=%208GNDI0hNQ4CAYxf6wNfkHF6&catid=50009529&cna=%208GNDI0hNQ4CAYxf6wNfkHF6&areaid=tad_first_area&_ksTS=1433488349382_761&callback=jsonp762&appid=152");
+		//String crawlPath = "/Users/apple/data/";
+		String crawlPath = "C:\\temp\\";
+		System.out.println("crawlPath: " + crawlPath);
+		
+		DemoJsonCrawler crawler = new DemoJsonCrawler(crawlPath);
+		crawler.addSeed("http://zhi.taobao.com/seller/json/getSigninDetail.htm?day=20150619&appId=1%2C7%2C9&_ksTS=1433824780590_75&callback=jsonp76");
 
 		crawler.start(1);
-
 	}
 
 }
